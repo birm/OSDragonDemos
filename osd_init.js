a38a6c2c646bbb223204d09eee84886358fb64bd
Ryan Birmingham <birm@rbirm.us>
2017-08-22 16:10:13 -0400

    add sample images

---
+++ osd_init.js
@@ -0,0 +1,22 @@
+var prefixurl = "img/duomo_files/";
+var tilesources = "img/duomo_files/"
+var viewer1 = OpenSeadragon({
+  id: "first",
+  prefixUrl: prefixurl,
+  tileSources: tilesources
+});
+var viewer2 = OpenSeadragon({
+  id: "second",
+  prefixUrl: prefixurl,
+  tileSources: tilesources
+});
+var viewer3 = OpenSeadragon({
+  id: "third",
+  prefixUrl: prefixurl,
+  tileSources: tilesources
+});
+var viewer4 = OpenSeadragon({
+  id: "fourth",
+  prefixUrl: prefixurl,
+  tileSources: tilesources
+});
