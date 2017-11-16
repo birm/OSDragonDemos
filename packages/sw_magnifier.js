3b77c8bcb8d247a2a67cff1532927623f3ce2f28
Ryan Birmingham <birm@rbirm.us>
2017-11-16 13:47:44 -0500

    try more flexible default zoom

--- packages/sw_magnifier.js
+++ packages/sw_magnifier.js
@@ -8,7 +8,7 @@ function sw_magnifier(viewer, modal_viewer) {
     // return a callback
     return function () {
         // set default to max zoom
-        document.getElementById(modal_viewer.id)['zoomlevel'] = modal_viewer.viewport.getMaxZoom();
+        document.getElementById(modal_viewer.id)['zoomlevel'] = document.getElementById(modal_viewer.id)['zoomlevel'] ||  modal_viewer.viewport.getMaxZoom();
         document.getElementById(modal_viewer.id).style.position = "absolute";
         var tracker = new OpenSeadragon.MouseTracker({
                 element: viewer.container,
