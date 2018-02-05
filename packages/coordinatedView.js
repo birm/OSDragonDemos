0a68d1ab8d4f72e61deb6b2e4d1ed3f77f9b6cca
Ryan Birmingham <birm@rbirm.us>
2018-02-05 15:46:47 -0500

    coordinated view demo

--- packages/coordinatedView.js
+++ packages/coordinatedView.js
@@ -1,33 +1,23 @@
-class coordinatedView {
+function coordinatedView(viewer1, viewer2, del_x, del_y) {
       /** Returns a callback which starts bidirectional view coordination
        * run with window.setTimeout((this).initalize(viewer1, viewer2),500);
        */
-      init(){
-       this.del_x = 0;
-       this.del_y = 0;
-      }
 
-      set_calibration(del_x, del_y){
-        this.del_x = del_x;
-        this.del_y = del_y;
-      }
-
-      static transmit(from_viewer, to_viewer){
-        var from_point = from_viewer.viewport.getCenter();
-        var dest_point = new OpenSeadragon.Point(from_point.x + this.del_x, from_point.y + this.del_y);
-        to_viewer.viewport.zoomTo(from_viewer.viewport.getZoom(), dest_point, true);
-        to_viewer.viewport.panTo(dest_point, true);
-      }
-
-      initalize(viewer1, viewer2){
-        // on any click on an element, transmit
-        return function(){
-          viewer1.addHandler("zoom", coordinatedView.transmit(viewer1,viewer2));
-          viewer1.addHandler("pan", coordinatedView.transmit(viewer1,viewer2));
-          viewer2.addHandler("zoom", coordinatedView.transmit(viewer2,viewer1));
-          viewer2.addHandler("pan", coordinatedView.transmit(viewer2,viewer1));
-        }.bind(this)
+      function initalize(){
+          function transmit(from_viewer, to_viewer){
+            return function(){
+              var from_point = from_viewer.viewport.getCenter();
+              var dest_point = new OpenSeadragon.Point(from_point.x + del_x, from_point.y + del_y);
+              to_viewer.viewport.zoomTo(from_viewer.viewport.getZoom(), dest_point, false);
+              to_viewer.viewport.panTo(dest_point, false);
+            }
+          }
+          var events=["click", "mouseover", "mousemove", "wheel", "keypress", "zoom", "pan"];
+          events.forEach(function(ev){
+            viewer1.container.addEventListener(ev ,transmit(viewer1,viewer2));
+            viewer2.container.addEventListener(ev ,transmit(viewer2,viewer1));
+          })
 
       }
-
+      return initalize;
 }
