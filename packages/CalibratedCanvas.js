28c5d7e1c8ff3b3a56ee2c0bd247ff74005eb27a
Ryan Birmingham <birm@rbirm.us>
2018-02-16 18:26:29 -0500

    start calibrated canvas

---
+++ packages/CalibratedCanvas.js
@@ -0,0 +1,9 @@
+class CalibratedCanvas{
+  init(canvas, ref, viewer){
+    this.base = base;
+    this.ref = ref;
+    this.viewer = viewer;
+  }
+  // method for coordinate conversion
+  // do all of the other methods for canvas
+}
